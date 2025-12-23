2a0f8ac1c108e07ea8b20eef293e86ab6e2c41ab
Elena Cester <elenna.cess@gmail.com>
2025-12-23 12:51:18 +0000

    icons added to home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButtons } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { heart, settings } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButtons],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({ heart, settings });
+  }
 }
