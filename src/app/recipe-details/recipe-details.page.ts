3fe4575bdbafe7af9f4bf1919147f175513ac55a
Elena Cester <elenna.cess@gmail.com>
2025-12-25 09:36:02 +0000

    Adding basic skeleton to recipe-details.html

--- src/app/recipe-details/recipe-details.page.ts
+++ src/app/recipe-details/recipe-details.page.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButtons, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle,IonCardContent, IonButton } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { heart } from 'ionicons/icons';
 import { SpoonacularApi } from '../services/spoonacular-api';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.page.html',
   styleUrls: ['./recipe-details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [FormsModule, CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButtons, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle,IonCardContent, IonButton]
 })
 export class RecipeDetailsPage implements OnInit {
 
@@ -22,6 +23,9 @@ export class RecipeDetailsPage implements OnInit {
   ngOnInit() {
     //Initialise receipe details with an empty array
     this.recipeData = [];
+    //Add icons
+    addIcons({ heart });
+
     this.showRecipeDetails(this.recipeId);
   }
 
